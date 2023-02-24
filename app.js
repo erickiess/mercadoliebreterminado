a6caa6834525e871bc146892f18a88928f3babc4
erickiess <erickiessling@gmail.com>
2023-02-24 00:30:39 -0300

    todo para render

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const path = require ("path");
 
 const app = express ();
 
+const port = process.env.PORT || 3000;
 
 
-app.listen(3000,()=> (
-    console.log("Servidor escuchando en el puerto 3000")
+app.listen(port,()=> (
+    console.log("Servidor escuchando en el puerto " + port  )
 ));
 app.use(express.static("public"));
     
